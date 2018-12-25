daac3f0f567357df805ae449e39e259da60b5329
ljx <123@qq.com>
2018-12-25 10:04:59 +0800

    22222

--- demo_test/src/main/java/com/itheima/bean/Test.java
+++ demo_test/src/main/java/com/itheima/bean/Test.java
@@ -3,5 +3,6 @@ package com.itheima.bean;
 public class Test {
     public static void main(String[] args) {
         System.out.println("111111111111");
+        System.out.println("222222222222");
     }
 }
